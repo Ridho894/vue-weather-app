dbbd9abd15546da383c7b44771f07c51b04b01f4
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-11 18:54:48 +0700

    sudah bisa menampilkan data cuaca hari ini

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,23 @@
   <div id="app">
     <main>
       <div class="search-box">
-        <input type="text" class="search-bar" placeholder="Search..." />
+        <input
+          type="text"
+          class="search-bar"
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
+        />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Northampton, UK</div>
+          <div class="location">{{ weather.name }}</div>
           <div class="date">Monday, 20 January 2020</div>
         </div>
         <div class="weather-box">
-          <div class="temp">35&deg;C</div>
-          <div class="weather">Rain</div>
+          <div class="temp">{{ Math.round(weather.main.temp) }}&deg;C</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -25,7 +31,26 @@ export default {
   data() {
     return {
       api_key: "0bc7ef28a9ee9f784cda17e64416e947",
+      url_base: "https://api.openweathermap.org/data/2.5/",
+      query: "",
+      weather: {},
     };
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key == "Enter") {
+        fetch(
+          `${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`
+        )
+          .then((res) => {
+            return res.json();
+          })
+          .then(this.setResults);
+      }
+    },
+    setResults(results) {
+      this.weather = results;
+    },
   },
 };
 </script>
