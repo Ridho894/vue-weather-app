7246f7d0daac8f49bc5a7ac05454c342b6978cd5
Ridho894 <77140390+Ridho894@users.noreply.github.com>
2021-09-11 09:40:40 +0700

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app"></div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      api_key: "0bc7ef28a9ee9f784cda17e64416e947",
+    };
+  },
+};
+</script>
+
+<style></style>
